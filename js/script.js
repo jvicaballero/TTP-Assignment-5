3f584fb679090fb5b0d9f733eee5cca788b42341
Jan Markus Milan <jmarkusm@me.com>
2020-06-01 16:48:22 -0400

    Initial commit to branch

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
-let cells = 1;
+let columns = 1;
 function addRow() {
     let table = document.getElementById("table");
     let row = document.createElement("tr");
     row.classList.add("row");
     table.appendChild(row);
-    for(let i = 0; i < cells; i++) {
+    for(let i = 0; i < columns; i++) {
         let cell = document.createElement("td");
         row.appendChild(cell);
     }
+}
+
+function removeRow() {
+    let row = document.getElementsByClassName("row");
+    tr = Array.from(row);
+    tr[tr.length - 1].parentNode.removeChild(tr[tr.length - 1]);
 }
